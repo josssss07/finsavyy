4a9e9e54fece7c4141f578317c5b83da9718483d
josssss07 <joshuadesai03@gmail.com>
2024-02-21 12:42:19 +0530

    ui scaffold made

--- lib/fin_litracy_section/litracy_home.dart
+++ lib/fin_litracy_section/litracy_home.dart
@@ -6,7 +6,71 @@ class LitracyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('FutureProof Yourself, Start Today'),
+        centerTitle: true,
+      ),
+      body: Column(
+        children:<Widget> [
       
+          Container(
+            height: 200,
+            width:350,
+            decoration: BoxDecoration(
+              color: Colors.red
+            ),
+            child: Text('text for test')
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                  height: 66,
+                  width: MediaQuery.of(context).size.width/3.1,
+                  decoration: BoxDecoration(
+                    color: Colors.red
+                  ),
+                  child: Text('test')
+                ),
+                Container(
+                  height: 66,
+                  width: MediaQuery.of(context).size.width/3.1,
+                  decoration: BoxDecoration(
+                    color: Colors.red
+                  ),
+                  child: Text('test')
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(5, 6, 5, 2),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                  height: 66,
+                  width: MediaQuery.of(context).size.width/3.1,
+                  decoration: BoxDecoration(
+                    color: Colors.red
+                  ),
+                  child: Text('test')
+                ),
+                Container(
+                  height: 66,
+                  width: MediaQuery.of(context).size.width/3.1,
+                  decoration: BoxDecoration(
+                    color: Colors.red
+                  ),
+                  child: Text('test')
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }

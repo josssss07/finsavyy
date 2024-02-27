9c84e2b98a9cb59722c580d33f295e451031bc8d
josssss07 <joshuadesai03@gmail.com>
2024-02-27 12:07:55 +0530

    login fix

--- lib/login/login_page.dart
+++ lib/login/login_page.dart
@@ -24,57 +24,59 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     TextEditingController username = TextEditingController();
     return Scaffold(
-        body: SingleChildScrollView(
-          child: Center(
-                child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          const Image(image: AssetImage('assets/images/welc.jpg')),
-          const Text(
-            "Empowering Financial Growth",
-            style: TextStyle(
-                fontSize: 30, color: Colors.black, fontWeight: FontWeight.w900),
-          ),
-          const Text(
-            "Bringing clarity to your financial roadmap.",
-            style: TextStyle(
-                fontSize: 15, color: Colors.black, fontWeight: FontWeight.normal),
-          ),
-          const SizedBox(
-            height: 60,
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(32, 10, 32, 18),
-            child: TextField(
-              controller: username,
-              decoration: InputDecoration(
-                  hintText: 'Enter your name',
-                  focusColor: Color.fromARGB(255, 47, 125, 121),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(12))),
+        body: Center(
+          child: SingleChildScrollView(
+            child: Center(
+                  child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            const Image(image: AssetImage('assets/images/welc.jpg')),
+            const Text(
+              "Empowering Financial Growth",
+              style: TextStyle(
+                  fontSize: 30, color: Colors.black, fontWeight: FontWeight.w900),
             ),
-          ),
-          Padding(
-              padding: EdgeInsets.all(18),
-              child: FloatingActionButton(
-                backgroundColor: const Color.fromARGB(255, 85, 145, 141),
-                onPressed: () {
-                  if(username.text.isEmpty){
-                    setState(() {
-                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content:Text('please enter a name')));
-                    });
-                  }else{
-                    String user = username.text;
-                    addData(user);
-                    Navigator.pushReplacement(
-                        context, MaterialPageRoute(builder: (_) => Bottom()));
-                  }
-                },
-                child: const Icon(
-                  Icons.arrow_right,
-                  color: Colors.black,
-                ),
-              ))
-                ]),
+            const Text(
+              "Bringing clarity to your financial roadmap.",
+              style: TextStyle(
+                  fontSize: 15, color: Colors.black, fontWeight: FontWeight.normal),
+            ),
+            const SizedBox(
+              height: 60,
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(32, 10, 32, 18),
+              child: TextField(
+                controller: username,
+                decoration: InputDecoration(
+                    hintText: 'Enter your name',
+                    focusColor: Color.fromARGB(255, 47, 125, 121),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12))),
               ),
+            ),
+            Padding(
+                padding: EdgeInsets.all(18),
+                child: FloatingActionButton(
+                  backgroundColor: const Color.fromARGB(255, 85, 145, 141),
+                  onPressed: () {
+                    if(username.text.isEmpty){
+                      setState(() {
+                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content:Text('please enter a name')));
+                      });
+                    }else{
+                      String user = username.text;
+                      addData(user);
+                      Navigator.pushReplacement(
+                          context, MaterialPageRoute(builder: (_) => Bottom()));
+                    }
+                  },
+                  child: const Icon(
+                    Icons.arrow_right,
+                    color: Colors.black,
+                  ),
+                ))
+                  ]),
+                ),
+          ),
         ));
   }
 }
